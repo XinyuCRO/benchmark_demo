af8a85924a55ae5ec2f0a9268bdac5d408943a28
Xinyu <xinyu.zhao@crypto.com>
2022-08-23 15:26:49 +0800

    feat: add influxDB service

--- monitor/src/main.ts
+++ monitor/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readBenchmarkConfig } from './utils/config';
-import { getLatestBlockNumber } from './web3';
-import { EVMMonitor } from './web3/EVMMonitor';
+import { readBenchmarkConfig } from 'src/utils/config';
+import { EVMMonitor } from 'src/service/web3/EVMMonitor';
+import { onBoardInfluxDB, writePoint } from './service/influxDB';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   // await app.listen(3000);
 
   const config = readBenchmarkConfig();
+
+  await onBoardInfluxDB();
+
+  await writePoint();
 
   const monitor = new EVMMonitor(config.provider);
 
